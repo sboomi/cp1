7f1cce0219079fbada21464647a3c6a67fd12c22
Shadi Boomi <shadi.boomi@gmail.com>
2021-03-03 11:49:31 +0100

    make bigger project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Etude de cas pratique n°1 pour la certification Simplon',
+    author='sboomi',
+    license='MIT',
+)
